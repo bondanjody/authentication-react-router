4c2ed7c1c8bdc5f1164df34f086b66ea89c7392c
bondanjody <bondanjodys@gmail.com>
2023-04-05 01:48:03 +0700

    Memperbaiki flaw yang timbul dari commit sebelumnya yaitu apabila user sudah melakukan login (misalnya 10 menit) lalu user tersebut melakukan refresh halaman yang mana akan berakibat pada session yang tadinya sudah berkurang (1 jam kurang 10 menit) akan kembali menjadi 1 jam lagi. Disini kita menggunakan localStorage untuk menampung durasi session user.

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -2,19 +2,29 @@ import { Outlet, useLoaderData, useSubmit } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
 import { useEffect } from 'react';
+import { getTokenDuration } from '../util/auth';
 
 function RootLayout() {
   const token = useLoaderData();
   const submit = useSubmit();
   // const navigation = useNavigation();
   useEffect(() => {
+
     if(!token) {
       return;
     }
 
+    if(token === 'EXPIRED') {
+      submit(null, {action: '/logout', method: 'post'});
+      return; 
+    }
+
+    const tokenDuration = getTokenDuration();
+    console.log(tokenDuration);
+
     setTimeout(() => {
       submit(null, {action: '/logout', method: 'post'})
-    }, 1*60*60*100);
+    }, tokenDuration);
   }, [token, submit])
 
   return (
